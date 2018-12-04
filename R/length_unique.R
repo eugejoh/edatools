4f16b5b1bca32087ef4ea9d9f3051b91eae394bc
[Eugene Joh] <[ejoh90@gmail.com]>
2018-12-04 14:14:29 -0500

    updated length_unique()

--- R/length_unique.R
+++ R/length_unique.R
@@ -1,10 +1,13 @@
-length_unique <- function(x,...) {
+length_unique <- function(x, kable=TRUE) {
   tot_row <- nrow(x)
   obj_name <- deparse(substitute(x))
   len_uni <- map_dfc(x, .f = ~length(unique(.x))) %>% 
     gather(key=Variable,value=`Counts`) %>% 
-    mutate(`Percentage of Total Rows` = round(100*Counts/tot_row,2)) %>% 
-    add_kable(caption = paste0("Number of Unique Values in '", obj_name, "'"))
+    mutate(`Percentage of Total Rows` = round(100*Counts/tot_row,2))
+  
+  if (kable) {
+    len_uni <- len_uni %>% add_kable(caption = paste0("Number of Unique Values in '", obj_name, "'"))
+  }
   
   return(len_uni) 
 }
